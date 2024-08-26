1b37ce725fd4f69020ab6f8dadeb5311cc13a188
James <66973299+Jimbob9191@users.noreply.github.com>
2024-08-26 17:55:29 +0100

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jimbob9191.github.io'
+});
